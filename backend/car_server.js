58a055615c4b55c57bd1ba65e5681edeb0349ad2
Melihhan <Melihhan@hotmail.co.uk>
2024-08-08 03:32:00 +0100

    updated carserver and versel

--- backend/car_server.js
+++ backend/car_server.js
@@ -1,6 +1,4 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import helmet from "helmet";
 import carRoutes from "./routes/cars.js";
 import config from "./config/config.js";
@@ -15,20 +13,16 @@ app.use(express.json());
 // Route middleware to handle API routes for car-related operations
 app.use("/api", carRoutes);
 
-// Serve static files and handle client-side routing in production environment
-if (process.env.NODE_ENV === "production") {
-  // Resolve __dirname for ES module
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  // Serve static files from the React frontend build directory
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  // Catch-all handler to return the React frontend's index.html file
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+// Only needed if not deploying on Vercel
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
 
 // Start the server and listen on the specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 export { app };
+
